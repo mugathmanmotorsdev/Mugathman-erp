ab69142f7a46a53eb7ef23539c1dc1524d9b2049
Ismail Hassan Yusuf <mugathmanmotors.dev@gmail.com>
2025-10-20 14:53:59 +0100

    fixed bugs

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -3,8 +3,6 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Footer from "@/components/Footer";
 import { useAuth } from "@/hooks/useauth";
 
