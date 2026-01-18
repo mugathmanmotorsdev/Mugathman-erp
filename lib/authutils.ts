1ae422275a33a61fc8e5d993b294cb22200f74f0
Ismail Hassan Yusuf <mugathmanmotors.dev@gmail.com>
2026-01-18 17:51:54 +0100

    emergency commit for git resolution

--- lib/authutils.ts
+++ lib/authutils.ts
@@ -1,4 +1,7 @@
 import { auth } from "./auth"
+import prisma from "./prisma"
+import { generateToken } from "./utils/token-generator"
+import { User } from "@generated/prisma"
 
 export async function requireAuth() {
   const session = await auth()
@@ -8,3 +11,13 @@ export async function requireAuth() {
   return session
 }
 
+export async function generateActivationToken(user: User) {
+  const token = generateToken()
+  const activationToken = await prisma.userActivationToken.create({
+    data: {
+      user_id: user.id,
+      token,
+    },
+  })
+  return activationToken
+}
