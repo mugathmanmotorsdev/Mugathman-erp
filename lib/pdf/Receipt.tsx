c149e3a8159933b38933b72fd0010f00c89c8747
Ismail Hassan Yusuf <mugathmanmotors.dev@gmail.com>
2026-05-05 02:01:46 +0100

    fixt lint error in Receipt.tsx

--- lib/pdf/Receipt.tsx
+++ lib/pdf/Receipt.tsx
@@ -208,6 +208,9 @@ const styles = StyleSheet.create({
     color: "#334155",
     width: 50,
   },
+  footerValue: {
+    color: "#64748b",
+  },
 });
 
 export function ReceiptPDF({ sale }: { sale: Sale }) {
